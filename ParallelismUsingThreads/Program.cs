15abafa95cbe1758928f069be7c60472682a93e2
tavisca-ysant <ysant@tavisca.in>
2019-08-10 10:11:31 +0530

    First commit

---
+++ ParallelismUsingThreads/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelismUsingThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            Task.Run(async () =>
+            {
+                var task1 =  PrintData();
+                var task2 =  SaveData();
+                await Task.WhenAll(task1, task2);
+            }).GetAwaiter().GetResult();
+            Console.WriteLine($"Total time taken {watch.ElapsedMilliseconds}");
+            Console.ReadKey(true);
+        }
+
+        private async static Task PrintData()
+        {
+            await Task.Delay(3000);
+            Console.WriteLine("In method Print Data");
+        }
+
+        private async static Task SaveData()
+        {
+            await Task.Delay(4000);
+        }
+    }
+}
